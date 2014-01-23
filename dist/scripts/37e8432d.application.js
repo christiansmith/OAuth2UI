0a4be007eccce3af555a1806e53548a3adead44c
Christian Smith <smith@anvil.io>
2014-01-23 14:03:35 -0800

    ensure user session is resolved in all protected routes

--- dist/scripts/37e8432d.application.js
+++ dist/scripts/37e8432d.application.js
@@ -3,24 +3,50 @@
 angular.module('OAuth2UI', ['OAuth2UI.controllers', 'OAuth2UI.services'])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
 
+    /**
+     * HTML5 mode
+     */
     $locationProvider.html5Mode(true);
     $locationProvider.hashPrefix = '!';
+
+
+    /**
+     * Register interceptors
+     */
+
     $httpProvider.interceptors.push('AuthenticationInterceptor');
+
+
+    /**
+     * Ensure session has been pinged
+     */
+
+    var ensureSession = ['$q', '$rootScope', 'User', function ($q, $rootScope, User) {
+      var deferred = $q.defer();
+
+      if ($rootScope.session) {
+        deferred.resolve(User);
+      } else {
+        User.session().then(function () {
+          $rootScope.session = true;
+          deferred.resolve(User);
+        })
+      }
+
+      return deferred.promise;
+    }]
+
+
+    /**
+     * Route definitions
+     */
 
     $routeProvider
       .when('/authorize', {
         templateUrl: 'views/authorize.html',
         controller: 'AuthorizeCtrl',
         resolve: {
-          User: ['$q', 'User', function ($q, User) {
-            var deferred = $q.defer();
-
-            User.session().then(function () {
-              deferred.resolve(User);
-            })
-
-            return deferred.promise;
-          }]
+          User: ensureSession
         }
       })
       .when('/signin', {
@@ -33,20 +59,23 @@ angular.module('OAuth2UI', ['OAuth2UI.controllers', 'OAuth2UI.services'])
       })
       .when('/account', {
         templateUrl: 'views/account.html',
-        controller: 'AccountCtrl'
+        controller: 'AccountCtrl',
+        resolve: {
+          User: ensureSession
+        }
       })
       .when('/account/apps', {
         templateUrl: 'views/apps.html',
-        controller: 'AppsCtrl'
+        controller: 'AppsCtrl',
+        resolve: {
+          User: ensureSession
+        }
       })
       .otherwise({
         redirectTo: '/signin'
       });
   })
 
-  .run(function (User) {
-    User.session()
-  })
 
 'use strict';
 
